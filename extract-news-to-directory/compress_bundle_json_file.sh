ea2b063abe0e435f1967768415780c41755d7982
hugo <hjbello.wk@gmail.com>
2020-04-13 12:11:11 +0200

    clean up old code

--- extract-news-to-directory/compress_bundle_json_file.sh
+++
@@ -1,5 +0,0 @@
-cd .. 
-cd data/json_news_tagged_bundle
-#gunzip -ck large-bundle-corona.json > reviews-180k-bundle-after-corona.json.gz
-zip reviews-400k-bundle-after-corona.zip large-bundle-corona.json 
-zip reviews-400k-bundle-after-corona-clean_data.zip clean_data.json 
