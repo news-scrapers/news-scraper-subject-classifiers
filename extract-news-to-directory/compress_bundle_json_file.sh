bfe4aa034797d04edd281178821bcca899d0a5a6
hugojbello <hjbello.wk@gmail.com>
2020-04-13 10:26:23 +0200

    new data

--- extract-news-to-directory/compress_bundle_json_file.sh
+++ extract-news-to-directory/compress_bundle_json_file.sh
@@ -1,5 +1,5 @@
 cd .. 
 cd data/json_news_tagged_bundle
 #gunzip -ck large-bundle-corona.json > reviews-180k-bundle-after-corona.json.gz
-zip reviews-280k-bundle-after-corona-large-tags.zip large-bundle-corona.json 
-zip reviews-280k-bundle-after-corona-large-tags-clean_data.zip clean_data.json 
+zip reviews-400k-bundle-after-corona-large-tags.zip large-bundle-corona.json 
+zip reviews-400k-bundle-after-corona-large-tags-clean_data.zip clean_data.json 
