ac499d9638f97648f0b95094af2dd09b3883e715
hugo <hjbello.wk@gmail.com>
2020-04-14 20:56:00 +0200

    fix

--- classifier-neural-network/tag_processor.py
+++ classifier-neural-network/tag_processor.py
@@ -11,7 +11,7 @@ import pickle
 class TagProcessor:
     def __init__(self):
         self.df = None
-        self.repetitions_threads_hole=600
+        self.repetitions_threads_hole=800
         self.main_tags = []
     
 
