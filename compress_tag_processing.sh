33115bc7e856a9a8fdb45766b4c525ec64e060a4
hugojbello <hjbello.wk@gmail.com>
2020-04-15 08:59:40 +0200

    new data

---
+++ compress_tag_processing.sh
@@ -0,0 +1,3 @@
+cd data/json_news_tagged_bundle
+#gunzip -ck large-bundle-corona.json > reviews-180k-bundle-after-corona.json.gz
+zip reviews-600k-bundleclean_data-unified-tags.zip clean_data-unified-tags.json 
