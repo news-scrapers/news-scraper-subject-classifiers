e4380fce89a636de0b0a48587800a76ceeae6ba4
Hugo Bello <hjbello.wk@gmail.com>
2020-04-16 20:25:10 +0200

    new modesl

--- compress_models.sh
+++ compress_models.sh
@@ -1,5 +1,5 @@
 
 cd data
 cd neural_network_config
-zip model_and_tokenizers_20200415.zip model.h5 model.json temp-model.h5 tokenizer.pickle 
+zip model_and_tokenizers_20200416.zip model.h5 model.json temp-model.h5 tokenizer.pickle 
 
